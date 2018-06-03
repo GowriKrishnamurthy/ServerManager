231bfcf53ceceb04c9fd98edb5691209f32013c0
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-03 19:11:16 +1000

    Basic user methods added

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  users = [
+    {
+      id: 1,
+      name: 'Smith'
+    },
+    {
+      id: 2,
+      name: 'Anna'
+    },
+    {
+      id: 3,
+      name: 'Chris'
+    }
+  ];
 
   constructor() { }
 
